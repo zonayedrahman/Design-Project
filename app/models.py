62e45aa64ffbc8413869ab98266c419d8e5cda88
zonayedrahman <zr578@nyu.edu>
2023-03-28 11:53:17 -0400

    Some stuff

--- app/models.py
+++ app/models.py
@@ -3,8 +3,15 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
+class Interest(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    interests = db.relationship('Interest')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Interest
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -11,3 +14,31 @@ def home():
         return render_template("home.html", user=current_user)
     return render_template("home.html", user=current_user)
 
+
+@views.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        return render_template("settings.html", user=current_user)
+    return render_template("settings.html", user=current_user)
+
+
+@views.route('/interests', methods=['GET', 'POST'])
+@login_required
+def interests():
+    if request.method == 'POST':
+        return render_template("interests.html", user=current_user)
+    return render_template("interests.html", user=current_user)
+
+
+@views.route('/delete-interest', methods=['POST'])
+def delete_note():
+    interest = json.loads(request.data) # this function expects a JSON from the INDEX.js file
+    interestId = interest['interestId']
+    note = Interest.query.get(interestId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
